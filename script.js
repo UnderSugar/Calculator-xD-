2a35e5efeaa967f6e1a57da1f7edddf2aadc205f
Andrii Biloivanenko <timtam4ik@gmail.com>
2024-06-11 21:03:11 +0200

    new styles and functional

--- script.js
+++ script.js
@@ -22,49 +22,84 @@ function display(obj) {
     console.log(obj);
     console.log(typeof(obj))
     console.log(document.getElementById('one'))
-    if(document.getElementById('one') == obj){
-        document.getElementById('myinput').value += 1; 
+    if (document.getElementById('myinput').value == '0' || document.getElementById('myinput').value == '')
+        {
+        if(document.getElementById('one') == obj){
+            document.getElementById('myinput').value = 1; 
+        }
+        if(document.getElementById('two') == obj){
+            document.getElementById('myinput').value = 2; 
+        }
+        if(document.getElementById('three') == obj){
+            document.getElementById('myinput').value = 3; 
+        }
+        if(document.getElementById('four') == obj){
+            document.getElementById('myinput').value = 4; 
+        }
+        if(document.getElementById('five') == obj){
+            document.getElementById('myinput').value = 5; 
+        }
+        if(document.getElementById('six') == obj){
+            document.getElementById('myinput').value = 6; 
+        }
+        if(document.getElementById('seven') == obj){
+            document.getElementById('myinput').value = 7; 
+        }
+        if(document.getElementById('eight') == obj){
+            document.getElementById('myinput').value = 8; 
+        }
+        if(document.getElementById('nine') == obj){
+            document.getElementById('myinput').value = 9; 
+        }
+        if(document.getElementById('zero') == obj){
+            document.getElementById('myinput').value =0; 
+        }
     }
-    if(document.getElementById('two') == obj){
-        document.getElementById('myinput').value += 2; 
-    }
-    if(document.getElementById('three') == obj){
-        document.getElementById('myinput').value += 3; 
-    }
-    if(document.getElementById('four') == obj){
-        document.getElementById('myinput').value += 4; 
-    }
-    if(document.getElementById('five') == obj){
-        document.getElementById('myinput').value += 5; 
-    }
-    if(document.getElementById('six') == obj){
-        document.getElementById('myinput').value += 6; 
-    }
-    if(document.getElementById('seven') == obj){
-        document.getElementById('myinput').value += 7; 
-    }
-    if(document.getElementById('eight') == obj){
-        document.getElementById('myinput').value += 8; 
-    }
-    if(document.getElementById('nine') == obj){
-        document.getElementById('myinput').value += 9; 
-    }
-    if(document.getElementById('zero') == obj){
-        document.getElementById('myinput').value += 0; 
-    }
-    if(document.getElementById('plus') == obj){
-        document.getElementById('myinput').value += '+'; 
-    }
-    if(document.getElementById('minus') == obj){
-        document.getElementById('myinput').value += '-'; 
-    }
-    if(document.getElementById('multiplicate') == obj){
-        document.getElementById('myinput').value += '*'; 
-    }
-    if(document.getElementById('division') == obj){
-        document.getElementById('myinput').value += '/'; 
-    }
-    if(document.getElementById('clear') == obj){
-        document.getElementById('myinput').value = ''; 
+    else {
+        if(document.getElementById('one') == obj){
+            document.getElementById('myinput').value += 1; 
+        }
+        if(document.getElementById('two') == obj){
+            document.getElementById('myinput').value += 2; 
+        }
+        if(document.getElementById('three') == obj){
+            document.getElementById('myinput').value += 3; 
+        }
+        if(document.getElementById('four') == obj){
+            document.getElementById('myinput').value += 4; 
+        }
+        if(document.getElementById('five') == obj){
+            document.getElementById('myinput').value += 5; 
+        }
+        if(document.getElementById('six') == obj){
+            document.getElementById('myinput').value += 6; 
+        }
+        if(document.getElementById('seven') == obj){
+            document.getElementById('myinput').value += 7; 
+        }
+        if(document.getElementById('eight') == obj){
+            document.getElementById('myinput').value += 8; 
+        }
+        if(document.getElementById('nine') == obj){
+            document.getElementById('myinput').value += 9; 
+        }
+        if(document.getElementById('zero') == obj){
+            document.getElementById('myinput').value += 0; 
+        }
+        if(document.getElementById('plus') == obj){
+            document.getElementById('myinput').value += '+'; 
+        }
+        if(document.getElementById('minus') == obj){
+            document.getElementById('myinput').value += '-'; 
+        }
+        if(document.getElementById('multiplicate') == obj){
+            document.getElementById('myinput').value += '*'; 
+        }
+        if(document.getElementById('division') == obj){
+            document.getElementById('myinput').value += '/'; 
+        }
+        if(document.getElementById('clear') == obj){
+            document.getElementById('myinput').value = '0'; 
+        }
     }
 }
